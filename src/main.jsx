9885e2e9923792b69cd762de51d6bf156706a67d
Rihards <eaa23riva@students.eaaa.dk>
2024-09-05 19:34:32 +0200

    Trying to fix github pages issue

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/react-my-first-project">
       <App />
     </BrowserRouter>
   </StrictMode>
